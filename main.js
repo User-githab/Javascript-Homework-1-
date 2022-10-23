8e66ea1e5bbd37a41cdc030fa6cfc303a5367495
Rustamjon <rustam.ergashev.98@mail.ru>
2022-10-23 15:43:29 +0500

    javascript

---
+++ main.js
@@ -0,0 +1,18 @@
+var imya = prompt('Введите своё имя');
+var age = +prompt('Введите свой возраст');
+var primer1 = +prompt('Решите пример 16 + 3 = ? ');
+var primer2 = +prompt('Решите пример 10 - 6 = ? ');
+var primer3 = +prompt('Решите пример 15 * 7 = ? ');
+var primer4 = +prompt('Решите пример 14 / 4 = ? ');
+var primer5 = +prompt('Решите пример 10 % 5 = ? ');
+var end = +prompt('Войдите в консоль для просмотра');
+
+
+console.log(' Меня зовут ' + imya);
+console.log(' Мне ' + age);
+console.log('Пример 1) 16 + 3 = ' + (16 + 3) + ' Ваш ответ ' + primer1);
+console.log('Пример 2) 10 - 6 = ' + (10 - 6) + ' Ваш ответ ' + primer2);
+console.log('Пример 3) 15 * 7 = ' + 15 * 7 + ' Ваш ответ ' + primer3);
+console.log('Пример 4) 14 / 4 = ' + 14 / 4 + ' Ваш ответ ' + primer4);
+console.log('Пример 5) 10 % 5 = ' + 10 % 5 + ' Ваш ответ ' + primer5);
+
